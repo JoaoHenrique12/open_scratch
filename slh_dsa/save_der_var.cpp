119606c3bc8046e5ec7e5c1bfd00c8aca9d671e1
Joao Henrique <joao.henrique1299@hotmail.com>
2025-09-20 17:19:32 -0300

    feat: add file to save keys on der format in variables

---
+++ slh_dsa/save_der_var.cpp
@@ -0,0 +1,115 @@
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <openssl/pem.h>
+#include <openssl/x509.h>   // PKCS8_PRIV_KEY_INFO
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+void handleErrors() {
+    ERR_print_errors_fp(stderr);
+    abort();
+}
+
+int main() {
+    OPENSSL_init_crypto(OPENSSL_INIT_ADD_ALL_DIGESTS, nullptr);
+    ERR_load_crypto_strings();
+
+    EVP_PKEY_CTX* keygen_ctx = nullptr;
+    EVP_PKEY* pkey = nullptr;
+
+    // Generate SLH-DSA-SHA2-128f
+    keygen_ctx = EVP_PKEY_CTX_new_from_name(nullptr, "SLH-DSA-SHA2-128f", nullptr);
+    if (!keygen_ctx) handleErrors();
+
+    if (EVP_PKEY_keygen_init(keygen_ctx) <= 0) handleErrors();
+    if (EVP_PKEY_keygen(keygen_ctx, &pkey) <= 0) handleErrors();
+
+    EVP_PKEY_CTX_free(keygen_ctx);
+
+    // --- Save Private Key in DER (PKCS#8) ---
+    std::vector<unsigned char> priv_der;
+    {
+        PKCS8_PRIV_KEY_INFO* p8inf = EVP_PKEY2PKCS8(pkey);
+        if (!p8inf) handleErrors();
+        int len = i2d_PKCS8_PRIV_KEY_INFO(p8inf, nullptr);
+        if (len <= 0) handleErrors();
+        priv_der.resize(len);
+        unsigned char* p = priv_der.data();
+        if (i2d_PKCS8_PRIV_KEY_INFO(p8inf, &p) != len) handleErrors();
+        PKCS8_PRIV_KEY_INFO_free(p8inf);
+        std::cout << "Private key (DER, PKCS#8) length: " << priv_der.size() << "\n";
+    }
+
+    // --- Save Public Key in DER (SubjectPublicKeyInfo) ---
+    std::vector<unsigned char> pub_der;
+    {
+        int len = i2d_PUBKEY(pkey, nullptr);
+        if (len <= 0) handleErrors();
+        pub_der.resize(len);
+        unsigned char* p = pub_der.data();
+        if (i2d_PUBKEY(pkey, &p) != len) handleErrors();
+        std::cout << "Public key (DER) length: " << pub_der.size() << "\n";
+    }
+
+    // --- Reconstruct Private Key from DER ---
+    EVP_PKEY* pkey_from_der = nullptr;
+    {
+        const unsigned char* p = priv_der.data();
+        PKCS8_PRIV_KEY_INFO* p8inf = d2i_PKCS8_PRIV_KEY_INFO(nullptr, &p, priv_der.size());
+        if (!p8inf) handleErrors();
+        pkey_from_der = EVP_PKCS82PKEY(p8inf);
+        PKCS8_PRIV_KEY_INFO_free(p8inf);
+        if (!pkey_from_der) handleErrors();
+    }
+
+    // --- Reconstruct Public Key from DER ---
+    EVP_PKEY* pub_from_der = nullptr;
+    {
+        const unsigned char* p = pub_der.data();
+        pub_from_der = d2i_PUBKEY(nullptr, &p, pub_der.size());
+        if (!pub_from_der) handleErrors();
+    }
+
+    // Message
+    const char* msg = "SLH-DSA test message";
+    size_t msg_len = strlen(msg);
+
+    // Signing with reconstructed private key
+    EVP_MD_CTX* mdctx = EVP_MD_CTX_new();
+    if (!mdctx) handleErrors();
+
+    if (EVP_DigestSignInit(mdctx, nullptr, nullptr, nullptr, pkey_from_der) <= 0) handleErrors();
+
+    size_t siglen = 0;
+    if (EVP_DigestSign(mdctx, nullptr, &siglen, (const unsigned char*)msg, msg_len) <= 0) handleErrors();
+
+    std::vector<unsigned char> sig(siglen);
+    if (EVP_DigestSign(mdctx, sig.data(), &siglen, (const unsigned char*)msg, msg_len) <= 0) handleErrors();
+
+    EVP_MD_CTX_free(mdctx);
+
+    // Verifying with reconstructed public key
+    mdctx = EVP_MD_CTX_new();
+    if (!mdctx) handleErrors();
+
+    if (EVP_DigestVerifyInit(mdctx, nullptr, nullptr, nullptr, pub_from_der) <= 0) handleErrors();
+
+    int verify_ok = EVP_DigestVerify(mdctx, sig.data(), siglen, (const unsigned char*)msg, msg_len);
+
+    if (verify_ok == 1) {
+        std::cout << "Signature verified successfully (with DER keys).\n";
+    } else if (verify_ok == 0) {
+        std::cout << "Signature verification failed.\n";
+    } else {
+        handleErrors();
+    }
+
+    EVP_MD_CTX_free(mdctx);
+    EVP_PKEY_free(pkey);
+    EVP_PKEY_free(pkey_from_der);
+    EVP_PKEY_free(pub_from_der);
+    ERR_free_strings();
+
+    return 0;
+}
